afe5f3eef0ab477b6071f41ce8485c2d5aaf9b25
manobhavmehta <manobhavmehta88@gmail.com>
2023-05-25 23:07:15 +0530

    Updates

---
+++ precache-manifest.20a7296a0855df2d031d648e38f1b0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "48a0d719bc2aed8da1ac",
+    "url": "/e-commerce/static/css/main.11bf3fb6.chunk.css"
+  },
+  {
+    "revision": "48a0d719bc2aed8da1ac",
+    "url": "/e-commerce/static/js/main.48a0d719.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/e-commerce/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "6b4ea7df01b11a6c2678",
+    "url": "/e-commerce/static/js/2.6b4ea7df.chunk.js"
+  },
+  {
+    "revision": "7ae9630d385bf40be22c",
+    "url": "/e-commerce/static/js/runtime~main.7ae9630d.js"
+  },
+  {
+    "revision": "05bd07557d51badd7d1f238c996042aa",
+    "url": "/e-commerce/index.html"
+  }
+];
